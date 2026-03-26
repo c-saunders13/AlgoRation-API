e3e94bdca4d33bab64ca493092b12b6d14ef47b1
Carl Saunders <41913276+c-saunders13@users.noreply.github.com>
2026-03-26 13:58:57 +0200

    Added repositories and interfaces

--- AlgoRationsProject/AlgoRationsAPI/Interfaces/IIngredientRepository.cs
+++ AlgoRationsProject/AlgoRationsAPI/Interfaces/IIngredientRepository.cs
@@ -4,9 +4,9 @@ namespace AlgoRationsAPI.Interfaces;
 
 public interface IIngredientRepository
 {
-  List<Ingredient> GetAll();
-  Ingredient GetById();
-  Ingredient Add();
-  Ingredient Update();
-  bool Delete();
+  IEnumerable<Ingredient> GetAll();
+  Ingredient? GetById(Guid id);
+  void Add(Ingredient ingredient);
+  Ingredient? Update(Ingredient ingredient);
+  Ingredient? Delete(Guid id);
 }
--- AlgoRationsProject/AlgoRationsAPI/Repositories/IngredientRepository.cs
+++ AlgoRationsProject/AlgoRationsAPI/Repositories/IngredientRepository.cs
@@ -1,7 +1,40 @@
 using AlgoRationsAPI.Interfaces;
+using AlgoRationsAPI.Models;
 
 namespace AlgoRationsAPI.Repositories;
 
-public class IngredientRepository
+public class IngredientRepository : IIngredientRepository
 {
+  private readonly List<Ingredient> _ingredients = [];
+
+  public IEnumerable<Ingredient> GetAll() => _ingredients;
+
+  public Ingredient? GetById(Guid id) => _ingredients.FirstOrDefault(i => i.Id == id);
+
+  public void Add(Ingredient ingredient)
+  {
+    ingredient.Id = Guid.NewGuid();
+    _ingredients.Add(ingredient);
+  }
+
+  public Ingredient? Update(Ingredient ingredient)
+  {
+    var existing = GetById(ingredient.Id);
+    if (existing != null)
+    {
+      existing.Name = ingredient.Name;
+      existing.AvailableQuantity = ingredient.AvailableQuantity;
+    }
+    return existing;
+  }
+
+  public Ingredient? Delete(Guid id)
+  {
+    var ingredient = GetById(id);
+    if (ingredient != null)
+    {
+      _ingredients.Remove(ingredient);
+    }
+    return ingredient;
+  }
 }
